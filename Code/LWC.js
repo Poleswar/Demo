f8bfdc7e714c015ab616ef4a747aad0f8915f9b4
Poleswar <95750813+Poleswar@users.noreply.github.com>
2021-12-08 16:53:50 +0530

    Create LWC.js

---
+++ Code/LWC.js
@@ -0,0 +1,73 @@
+import {LightningElement} from 'lwc';
+
+import {NavigationMixin} from 'lightning/navigation';
+
+export default class NavigationComponent extends NavigationMixin(LightningElement) {
+
+
+
+    NavigatetonewLead() {
+
+   this[NavigationMixin.Navigate]({
+
+       type: 'standard__objectPage',
+
+           attributes: {
+
+           recordId:this.recordId,
+
+           objectApiName: 'Lead',
+
+           actionName: 'new'
+
+       },
+
+    });
+
+ }
+
+ 
+
+ NavigatetoeditAccount() {
+
+    this[NavigationMixin.Navigate]({
+
+        type: 'standard__recordPage',
+
+            attributes: {
+
+            recordId: '0015g00000HjHI6AAN',
+
+            objectApiName: 'Account',
+
+            actionName: 'edit'
+
+        },
+
+     });
+
+  }
+
+  NavigatetonewComponent(){
+
+    this[NavigationMixin.Navigate]({
+
+            "type": "standard__component",
+
+            "attributes": {
+
+               
+
+                "componentName": "c__myComponent"
+
+            }
+
+        });
+
+
+
+    }
+
+ 
+
+}
