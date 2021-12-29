161d56fbba57b40a6aaae7d711ed15c1bbbcebea
Poleswar <95750813+Poleswar@users.noreply.github.com>
2021-12-29 12:32:28 +0530

    Add files via upload

---
+++ fetchUrl.js
@@ -0,0 +1,25 @@
+import { LightningElement, api, wire, track} from'lwc';
+import CURRENTUSERID from '@salesforce/user/Id';
+import fetchBaseURL from '@salesforce/apex/Fetchurl.fetchBaseURL';
+export default class FetchRecordId extends LightningElement {
+    @api recordId;
+    @api objectApiName;
+    @track link;
+    @track hlink;
+    @wire(fetchBaseURL)
+    wiredAccounts({ error, data }) {
+        if(data) {
+            //console.log(data);
+            this.link = data;
+            console.log(data);
+        } else if (error) {
+            console.log(error);
+            this.error = error;
+        }     
+    }
+    handleString(evt){
+        console.log('test***********************'+this.link);
+        this.hlink =this.link +"/" +this.objectApiName+"/"+this.recordId;
+        console.log('8888888888888888888888888'+this.hlink);
+    }
+}
